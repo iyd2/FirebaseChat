ec39b90fd4471fa58b0f2f8383144cf0c28cbbbe
ivan <fran.totti_96@mail.ru>
2018-06-25 23:49:52 +0300

    Add new navigation host interface

--- app/src/main/java/iyd2/projects/firebasechatclient/NavigationHost.java
+++ app/src/main/java/iyd2/projects/firebasechatclient/NavigationHost.java
@@ -1,4 +1,12 @@
 package iyd2.projects.firebasechatclient;
 
+import android.support.v4.app.Fragment;
+
 public interface NavigationHost {
+
+    /**
+     * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
+     * stack to make this navigation reversible.
+     */
+    void navigateTo(Fragment fragment, boolean addToBackstack);
 }
