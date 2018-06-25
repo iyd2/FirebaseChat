44ad32103099cb527b85b2c63eea56d5ed97bcb8
ivan <fran.totti_96@mail.ru>
2018-06-25 23:53:40 +0300

    Add hosting fragmets activity

--- app/src/main/java/iyd2/projects/firebasechatclient/MainActivity.java
+++ app/src/main/java/iyd2/projects/firebasechatclient/MainActivity.java
@@ -1,13 +1,39 @@
 package iyd2.projects.firebasechatclient;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationHost {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chat_main_activity);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        if (savedInstanceState == null) {
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, LoginFragment.newInstance())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction = getSupportFragmentManager()
+                                            .beginTransaction()
+                                            .replace(R.id.fragment_container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+
     }
 }
